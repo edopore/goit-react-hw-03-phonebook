1534a53ec385ba0856d197df057e6e1fafaabc82
Eduardo Jose Maya Rodriguez <ediij1994@gmail.com>
2024-03-30 18:23:23 -0500

    UPDATE phonebook done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,38 +3,59 @@ import Contacts from './contacts/Contacts';
 import ContactForm from './contactForm/ContactForm';
 import Filter from './filter/Filter';
 import './App.css';
-import { contactsDb } from './contacts.db';
 
 export class App extends Component {
-  state = {
-    contacts: [...contactsDb],
-    filter: '',
-  };
+  constructor() {
+    super();
+    this.state = {
+      contacts: [],
+      filter: '',
+    };
+  }
+
+  componentDidMount() {
+    if (localStorage.getItem('contacts') === null)
+      localStorage.setItem('contacts', JSON.stringify([]));
+    this.setState({
+      contacts: JSON.parse(localStorage.getItem('contacts')),
+    });
+  }
+
   addContact = contact => {
-    const result = contactsDb.filter(word =>
+    const json = localStorage.getItem('contacts');
+    const items = JSON.parse(json);
+    const result = items.filter(word =>
       word.name.toLowerCase().includes(contact.name.toLowerCase())
     );
     if (result.length > 0) return alert(`${result[0].name} already exists`);
-    contactsDb.push(contact);
+    items.push(contact);
+    localStorage.setItem('contacts', JSON.stringify(items));
     this.setState({
-      contacts: [...contactsDb],
+      contacts: [...items],
     });
   };
 
   findContact = param => {
-    const result = contactsDb.filter(word =>
+    const json = localStorage.getItem('contacts');
+    const items = JSON.parse(json);
+    const result = items.filter(word =>
       word.name.toLowerCase().includes(param)
     );
-    if (param === '') return this.setState({ contacts: contactsDb });
+    if (param === '') return this.setState({ contacts: items });
     this.setState({ contacts: result });
   };
+
   deleteContact = id => {
-    const result = contactsDb.findIndex(contact => contact.id === id);
-    contactsDb.splice(result, 1);
+    const json = localStorage.getItem('contacts');
+    const items = JSON.parse(json);
+    const result = items.findIndex(contact => contact.id === id);
+    items.splice(result, 1);
+    localStorage.setItem('contacts', JSON.stringify(items));
     this.setState({
-      contacts: [...contactsDb],
+      contacts: [...items],
     });
   };
+
   render() {
     return (
       <div className="app">
